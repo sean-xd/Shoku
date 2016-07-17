67847103cd298210114fafe025c3945019349f45
Sean Murray <thecanbeat@gmail.com>
2016-07-17 12:58:05 -0600

    added locations to stackoverflow

--- db/stackoverflow.js
+++ db/stackoverflow.js
@@ -1,18 +1,21 @@
 var parseURL = require("rss-parser").parseURL,
   getTags = require("./getTags.js");
 
-module.exports = function stackOverflow(url, magic){
+module.exports = function stackoverflow(url, magic){
   parseURL(url, (err, data) => {
     var result = data.feed.entries.map(e => {
-      var date = new Date(e.pubDate).getTime(),
+      var loc = !!e.title.split(" at ")[1].split(" (")[1],
+        date = new Date(e.pubDate).getTime(),
         titleSplit = e.title.split(" at "),
         title = titleSplit[0].split(" (")[0],
-        company = titleSplit[1].split(" (")[0],
+        titleSplit2 = titleSplit[1].split(" ("),
+        company = titleSplit2[0],
+        location = loc ? titleSplit2[1].slice(0,-1) : e.location ? e.location : "N/A",
         content = e.content,
         url = e.link,
         source = "stackoverflow",
         tags = getTags({title, content});
-      return {date, title, company, content, url, source, tags};
+      return {date, title, company, content, url, source, location, tags};
     });
     result = result.filter(e => !/ä|ö|ü|Ü|ß|ntwick/.test(e.title + e.company + e.content));
     magic(result);
