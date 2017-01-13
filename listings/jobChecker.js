0a7ddab108f4a76f7ea0a0253308ec0931a8f3bd
Sean Murray <thecanbeat@gmail.com>
2017-01-12 20:46:36 -0500

    more complexity reduction

--- listings/jobChecker.js
+++ listings/jobChecker.js
@@ -1,9 +1,16 @@
 /** @module jobChecker */
-module.exports = function jobChecker(job, list){
-  var brokenProps = !job.company || !job.content || !job.date || !job.location || !job.source || !job.title || !job.url,
-    brokenCompany = job.company.length < 2,
-    brokenDate = Date.now() - job.date > 1000 * 3600 * 24 * 30 || job.date > Date.now(),
-    isGerman = /ä|ö|ü|Ü|ß|ntwick/.test(job.title + job.company + job.content);
-  if(brokenProps || brokenCompany || brokenDate || isGerman) return list;
+module.exports = jobChecker;
+
+function jobChecker(job, list){
+  var isGerman = /ä|ö|ü|Ü|ß|ntwick/.test(job.title + job.company + job.content);
+  if(brokenProps(job) || job.company.length < 2 || brokenDate(job) || isGerman) return list;
   return list.concat([job]);
+}
+
+function brokenProps(job){
+  return !job.company || !job.content || !job.date || !job.location || !job.source || !job.title || !job.url;
+}
+
+function brokenDate(job){
+  return Date.now() - job.date > 1000 * 3600 * 24 * 30 || job.date > Date.now();
 }
