7205be2502eac542b13d24e242f5e2579e5b88a7
Sean Murray <thecanbeat@gmail.com>
2017-01-12 20:41:54 -0500

    trying to reduce complexity of jobchecker

--- listings/jobChecker.js
+++ listings/jobChecker.js
@@ -1,7 +1,9 @@
 /** @module jobChecker */
 module.exports = function jobChecker(job, list){
-  if(!job.company || !job.content || !job.date || !job.location || !job.source || !job.title || !job.url) return list;
-  if(job.company.length < 2 || Date.now() - job.date > 1000 * 60 * 60 * 24 * 30 || job.date > Date.now()) return list;
-  if(/ä|ö|ü|Ü|ß|ntwick/.test(job.title + job.company + job.content)) return list;
+  var brokenProps = !job.company || !job.content || !job.date || !job.location || !job.source || !job.title || !job.url,
+    brokenCompany = job.company.length < 2,
+    brokenDate = Date.now() - job.date > 1000 * 3600 * 24 * 30 || job.date > Date.now(),
+    isGerman = /ä|ö|ü|Ü|ß|ntwick/.test(job.title + job.company + job.content);
+  if(brokenProps || brokenCompany || brokenDate || isGerman) return list;
   return list.concat([job]);
 }
