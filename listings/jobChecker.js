c176931ddd607834acbf5cd3faff29376dcb9e5c
Sean Murray <thecanbeat@gmail.com>
2017-01-12 20:56:57 -0500

    hopefully 6 complexity is the limit

--- listings/jobChecker.js
+++ listings/jobChecker.js
@@ -3,12 +3,16 @@ module.exports = jobChecker;
 
 function jobChecker(job, list){
   var isGerman = /ä|ö|ü|Ü|ß|ntwick/.test(job.title + job.company + job.content);
-  if(brokenProps(job) || job.company.length < 2 || brokenDate(job) || isGerman) return list;
+  if(brokenProps(job) || brokenCompany(job) || brokenDate(job) || isGerman) return list;
   return list.concat([job]);
 }
 
+function brokenCompany(job){
+  return !job.company || job.company.length < 2;
+}
+
 function brokenProps(job){
-  return !job.company || !job.content || !job.date || !job.location || !job.source || !job.title || !job.url;
+  return !job.content || !job.date || !job.location || !job.source || !job.title || !job.url;
 }
 
 function brokenDate(job){
